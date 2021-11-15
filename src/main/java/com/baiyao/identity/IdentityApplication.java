72c9191ae53d717db19461e6923e8331b7c06030
wangbx <wangboxiao@gigacloudtech.com>
2021-11-15 19:35:26 +0800

    初步提交

--- src/main/java/com/baiyao/identity/IdentityApplication.java
+++ src/main/java/com/baiyao/identity/IdentityApplication.java
@@ -1,9 +1,19 @@
 package com.baiyao.identity;
 
+import com.alibaba.nacos.api.annotation.NacosProperties;
+import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySources;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@NacosPropertySources(value = {@NacosPropertySource(dataId = "system-config.yaml", autoRefreshed = true),
+        @NacosPropertySource(dataId = "example", autoRefreshed = true)
+})
+@EnableNacosConfig(globalProperties = @NacosProperties(serverAddr = "baiyao:8848", namespace = "53f890e7-3291-46c4-93c9-df2c100c1c9c"))
+@MapperScan("com.baiyao.identity.mapper")
 public class IdentityApplication {
 
     public static void main(String[] args) {
